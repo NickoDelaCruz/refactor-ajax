cf4c1728ff5495383a5a1a7fa0e7eb6825d7765e
Nicko DC <nickodc96@gmail.com>
2018-05-06 21:31:07 -0700

    Add flash messages in login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+
 class SessionsController < ApplicationController
 
   def new
@@ -7,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:alert]="You have successfully signed in"
       redirect_to '/'
     else
       redirect_to '/sign_in'
@@ -15,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:alert]="you have successfuly signed out"
     redirect_to '/sign_in'
   end
 
